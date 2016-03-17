c047e828102ea7179a0e90e5472d176b1168fa8a
Azmond <xdimon007x@gmail.com>
2016-03-17 19:47:17 +0300

    Add Project

---
+++ Analizator/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Analizator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripSeparator1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1227, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(73, 22);
+            this.toolStripButton1.Text = "Выполнить";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(613, 315);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "Начало\nАнализ\n42.56,32.12\nКонец анализа;\nСинтез\n11.44,22.11\nКонец синтеза\nсм12 =:" +
+    " - 13 + 23 * 13 or 43 / 2 +12\nКонец";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 567);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1227, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Visible = false;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Visible = false;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(619, 28);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(613, 315);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = resources.GetString("richTextBox2.Text");
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(0, 349);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1227, 215);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1227, 589);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Анализатор";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Analizator/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LanguageLibrary;
+
+namespace Analizator
+{
+    public partial class Form1 : Form
+    {
+        public Lexer LexerProcess;
+        public LogError ErrorProcess;
+        public Syntax SyntaxProcess;
+        public Execute Compiller;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;
+            listView1.Columns.Add("Строка", listView1.Width / 22);
+            listView1.Columns.Add("Символ", listView1.Width / 22);
+            listView1.Columns.Add("Анализатор", listView1.Width / 12);
+            listView1.Columns.Add("Сообщение", (listView1.Width / 3)*2);
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Count() > 0)
+            {
+                toolStripProgressBar1.Visible = true;
+                toolStripStatusLabel1.Visible = true;
+                toolStripStatusLabel1.Text = " Лексический анализ ....";
+                LexerProcess = new Lexer(richTextBox1.Text);
+                toolStripProgressBar1.Value = 34;
+                ErrorProcess = new LogError();
+                ErrorProcess.ParseLexemError(LexerProcess.ErrorLexems.ToList());
+                SyntaxProcess = new Syntax(LexerProcess.Lexems, ErrorProcess);
+                toolStripStatusLabel1.Text = " Синтаксический анализ ....";
+                toolStripProgressBar1.Value = 65;
+                LoadError(ErrorProcess);
+                foreach(string st in SyntaxProcess.Variable)
+                {
+                    SyntaxProcess.resultat.result=SyntaxProcess.resultat.result.Replace(st, "1");
+                }
+                if (ErrorProcess.ListError.Count == 0)
+                {
+                    toolStripStatusLabel1.Text = " Компиляция ....";
+                    Compiller = new Execute(listView1);
+                    Compiller.execute(SyntaxProcess.resultat, SyntaxProcess.varname);
+                    toolStripProgressBar1.Value = 100;
+                    toolStripProgressBar1.Visible = false;
+                    toolStripStatusLabel1.Text = " Компиляция успешно выполнена!";
+                }
+            }
+        }
+
+
+        private void LoadError(LogError err)
+        {
+            string temp2;
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, FontStyle.Regular);
+            richTextBox1.SelectionColor = System.Drawing.SystemColors.WindowText;
+            listView1.Items.Clear();
+            for (int i = 0; i < err.ListError.Count; i++)
+            {
+                richTextBox1.Select(err.ListError[i].ErrorLexem.Offset-err.ListError[i].ErrorLexem.Length, err.ListError[i].ErrorLexem.Length);
+                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, FontStyle.Underline);
+                richTextBox1.SelectionColor = Color.Red;
+                string temp1 = err.SourceType[err.ListError[i].ErrorSource].ToString();
+                
+                if(err.ListError[i].ErrorCode==1)
+                    temp2 = err.ErrorsMessage[err.ListError[i].ErrorCode].ToString()+" '"+err.ListError[i].ErrorLexem.Value+"'";
+                else
+                    temp2 = err.ErrorsMessage[err.ListError[i].ErrorCode].ToString();
+                
+                listView1.Items.Add((err.ListError[i].ErrorLexem.Row+1).ToString());
+                listView1.Items[i].SubItems.Add((err.ListError[i].ErrorLexem.Offset - err.ListError[i].ErrorLexem.Length).ToString());
+                listView1.Items[i].SubItems.Add(temp1);
+                listView1.Items[i].SubItems.Add(temp2);
+                listView1.Items[i].SubItems.Add(err.ListError[i].ErrorLexem.Length.ToString());
+            }
+            richTextBox1.Select(0, 0);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Select(ErrorProcess.ListError[listView1.FocusedItem.Index].ErrorLexem.Offset
+                -ErrorProcess.ListError[listView1.FocusedItem.Index].ErrorLexem.Length,
+                ErrorProcess.ListError[listView1.FocusedItem.Index].ErrorLexem.Length);
+            richTextBox1.Focus();   
+            
+        }
+
+    }
+}
